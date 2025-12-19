e04893c2d6f0b201ea6cd7b69d41d0ae04d75ba7
kektor <alexvas06@gmail.com>
2025-12-19 12:04:59 +0300

    Configured CI-CD workflow for activity service. pipeline : build -> parallel [unit tests/ it tests] -> sonar analyse -> docker build and push

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     kotlin("kapt") version "2.3.0"
     id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
+    id("jacoco")
+    id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "kektor.innowise.gallery"
@@ -33,10 +35,74 @@ dependencies {
     val mapstructVersion = "1.6.3"
     implementation("org.mapstruct:mapstruct:$mapstructVersion")
     kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+
+        val integrationTest by registering(JvmTestSuite::class) {
+            useJUnitJupiter()
+
+            dependencies {
+                implementation(project())
+                implementation("org.springframework.boot:spring-boot-starter-test")
+            }
+
+            targets {
+                all {
+                    testTask.configure {
+                        shouldRunAfter(test)
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+
+    dependsOn(tasks.test, testing.suites.named("integrationTest"))
+
+    executionData.setFrom(
+        fileTree(layout.buildDirectory).include("jacoco/*.exec")
+    )
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.organization", "cannibal-kektor")
+        property("sonar.projectKey", "cannibal-kektor_gallery-activity-service")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
+        )
+        property(
+            "sonar.junit.reportPaths",
+            "${layout.buildDirectory.get()}/test-results/test, " +
+                    "${layout.buildDirectory.get()}/test-results/integrationTest"
+        )
     }
 }
